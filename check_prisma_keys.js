2d52535085cbf27631e5a8e6713b8d698a1ef0cf
Mgrdegh Ghazarian <mgrdegh50@gmail.com>
2026-01-23 22:49:49 +0100

    chore: final cleanup

--- check_prisma_keys.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-    const keys = Object.keys(prisma).filter(k => !k.startsWith('$') && !k.startsWith('_'));
-    console.log('Prisma keys:', keys);
-}
-
-main();
